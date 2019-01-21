0ec6b21a0080c7ce54e56e6672dfb0603de658c1
dineshkumar <dinesh.er007@gmail.com>
2019-01-21 19:38:35 +0530

    ionic 2 camra access

---
+++ src/pages/home/home.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  public photos: any;
+  base64Image: string;
+  alertCtrl: any;
+
+  constructor(public navCtrl: NavController,
+    private camera: Camera
+  ) { }
+  ngOnInit() {
+    this.photos = [];
+  }
+  deletePhoto(index) {
+    let confirm = this.alertCtrl.create({
+        title: 'Sure you want to delete this photo? There is NO undo!',
+        message: '',
+        buttons: [
+          {
+            text: 'No',
+            handler: () => {
+              console.log('Disagree clicked');
+            }
+          }, {
+            text: 'Yes',
+            handler: () => {
+              console.log('Agree clicked');
+              this.photos.splice(index, 1);
+            }
+          }
+        ]
+      });
+    confirm.present();
+  }
+  takePhoto() {
+    const options: CameraOptions = {
+      quality: 50, // picture quality
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    this.camera.getPicture(options).then((imageData) => {
+      this.base64Image = "data:image/jpeg;base64," + imageData;
+      this.photos.push(this.base64Image);
+      this.photos.reverse();
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+
+
+}
